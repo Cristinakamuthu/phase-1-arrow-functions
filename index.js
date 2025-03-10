3cb677cd6bb26b45a36ad0e35c2f6e53e6644213
Cristina <kamuthucristina@gmail.com>
2025-03-10 12:45:58 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+ function divide (){
+    let a = 2000
+    let b = 100
+    return(a/b)
+ }
+ console.log(divide)
+
+ const square = () => {
+    let x = 2
+    return ( x*x)
+
+ }
+ console.log (square)
+ const add = () => {
+    let a = 3
+    let b = 4
+    return (a+b)
+ }
+ console.log (add)
+
